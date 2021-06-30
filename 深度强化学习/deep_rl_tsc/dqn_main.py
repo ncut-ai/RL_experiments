16b5e36813e15185231ba24a7b85b7c9388616ce
ncut-ai <1053940@qq.com>
2021-06-30 10:20:07 +0800

    zzd

--- 深度强化学习/deep_rl_tsc/dqn_main.py
+++ 深度强化学习/deep_rl_tsc/dqn_main.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
     env_setting, dqn_settings = get_settings_from_yaml(yaml_file='_config.yaml')
     # to initialize SUMO
     sumo_environment = TrafficEnvironment(env_setting=env_setting)  # traffic environment
+    
+    sumo_environment.simulation_step()
+    
+    sumo_environment.pre_run_simulation_to_prepare(10000)
